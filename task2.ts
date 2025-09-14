bce8e16b53ef8cfe8ae531071e27bbb7481ee6dc
Сапелко Сергей Игоревич <sopelmurman@gmail.com>
2025-09-14 11:52:48 +0300

    result univer solution task 4

--- task2.ts
+++ task2.ts
@@ -1,3 +1,39 @@
+/*
+
+Реализуйте хук useLocalStorage(), который можно будет использовать следующим образом:
+
+import { useLocalStorage } from './useLocalStorage';
+
+function Demo() {
+  const [value, { setItem, removeItem }] = useLocalStorage('some-key');
+
+  return (
+    <div>
+      <p>Значение из LocalStorage: {value}</p>
+      <div>
+        <button onClick={() => setItem('new storage value')}>Задать значение</button>
+        <button onClick={() => removeItem()}>Удалить значение</button>
+      </div>
+    </div>
+  );
+}
+Кроме того, необходимо добавить типизацию хука:
+
+type LocalStorageSetValue = string;
+type LocalStorageReturnValue = LocalStorageSetValue | null;
+
+type UseLocalStorage = (key: string) => [
+  value: LocalStorageReturnValue,
+  {
+    setItem: (value: LocalStorageSetValue) => void;
+    removeItem: () => void;
+  },
+];
+Здесь мы учитываем, что в LocalStorage значения всегда хранятся в виде строк. Однако в случае, если значение по ключу key не найдено, то вернется null. 
+
+*/
+
+
 import { useState } from 'react';
 
 type LocalStorageSetValue = string;
